c4f5c726c8c797763b5b2f1261384068a7541140
Julia Bullard <5534@holbertonstudents.com>
2024-03-14 10:53:12 -0500

    Fixed Task 6. Used client.copyToLocal().

--- hadoop_project/6-download.py
+++ hadoop_project/6-download.py
@@ -9,12 +9,5 @@ def download(l):
     # Create a client
     client = Client('localhost', 9000)
 
-    try:
-        for file_path in l:
-            # Open the file in HDFS for reading
-            with client.open(file_path) as reader:
-                # Read the data and write it to a local file
-                with open('/tmp/' + file_path.split('/')[-1], 'wb') as writer:
-                    writer.write(reader.read())
-    except Exception:
-        pass
+    for file_path in client.copyToLocal(l, '/tmp'):
+        print(file_path)
